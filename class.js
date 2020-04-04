ea8a9b2333e207f06b53981555d4e28bf0b1c3a7
unknown <eric,maloney@centre.edu>
2020-04-04 17:33:40 -0400

    classdatalab1

---
+++ class.js
@@ -0,0 +1,10 @@
+var studentPromise= d3.json("classData.json");
+studentPromise.then (
+var Sucess = function(penguin) 
+{ console.log ("worked", picture);
+    } 
+var Fail = function(Err) 
+{
+ console.log ("failed", Error);  
+}
+)
